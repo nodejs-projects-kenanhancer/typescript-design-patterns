474da3851b250ee4f69e477b41abd1c2d2567e85
kenanhancer <kenanhancer@gmail.com>
2023-03-23 20:52:32 +0000

    new command example is added

---
+++ src/command-pattern/example1/app.ts
@@ -0,0 +1,99 @@
+interface Switchable {
+  switchOn(): void;
+  switchOff(): void;
+}
+
+interface Command {
+  execute(): void;
+}
+
+class Light implements Switchable {
+  private static LIGHT_ON = "The light is on!";
+  private static LIGHT_OFF = "The light is off!";
+  private static LIGHT_ALREADY_ON = "The light is already turned on!";
+  private static LIGHT_ALREADY_OFF = "The light is already turned off!";
+
+  private lightState: boolean = false;
+
+  private setState(state: boolean, warning: string, notification: string) {
+    if (this.lightState === state) {
+      console.warn(warning);
+    } else {
+      this.lightState = state;
+      console.log(notification);
+    }
+  }
+
+  switchOn(): void {
+    this.setState(true, Light.LIGHT_ALREADY_ON, Light.LIGHT_ON);
+  }
+
+  switchOff(): void {
+    this.setState(false, Light.LIGHT_ALREADY_OFF, Light.LIGHT_OFF);
+  }
+}
+
+class SwitchOnCommand implements Command {
+  private readonly switchable: Switchable;
+
+  constructor(switchable: Switchable) {
+    this.switchable = switchable;
+  }
+
+  execute(): void {
+    this.switchable.switchOn();
+  }
+}
+
+class SwitchOffCommand implements Command {
+  private readonly switchable: Switchable;
+
+  constructor(switchable: Switchable) {
+    this.switchable = switchable;
+  }
+
+  execute(): void {
+    this.switchable.switchOff();
+  }
+}
+
+class SwitchBox {
+  private readonly switchOnCommand: Command;
+  private readonly switchOffCommand: Command;
+
+  constructor(switchOnCommand: Command, switchOffCommand: Command) {
+    this.switchOnCommand = switchOnCommand;
+    this.switchOffCommand = switchOffCommand;
+  }
+
+  open() {
+    this.switchOnCommand.execute();
+  }
+
+  close() {
+    this.switchOffCommand.execute();
+  }
+}
+
+(function () {
+  const light: Switchable = new Light();
+
+  const switchOnCommand: Command = new SwitchOnCommand(light);
+  const switchOffCommand: Command = new SwitchOffCommand(light);
+
+  const switchBox = new SwitchBox(switchOnCommand, switchOffCommand);
+
+  switchBox.open();
+
+  switchBox.open();
+
+  switchBox.close();
+
+  switchBox.close();
+
+  switchBox.open();
+
+  switchBox.close();
+})();
+
+export {};
